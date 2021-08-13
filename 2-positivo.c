041e8914063a57e9421642493bf2c2cf73fc0876
smokys <samuelviana2626@gmail.com>
2021-08-13 15:57:12 -0400

    segunda questão

--- 2-positivo.c
+++ 2-positivo.c
@@ -1,3 +1,6 @@
+//Leia um numero fornecido pelo usuario. Se esse numero for positivo, calcule a raiz
+//quadrada do numero. Se o numero for negativo, mostre uma mensagem dizendo que o
+//numero  e invalido
 #include "stdio.h"
 #include "math.h"
 int main(){
